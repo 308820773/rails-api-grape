3c19fff8a75e7f2db22cab4dc1de82858c2c89bb
black <black@paimingjia.com>
2020-02-19 09:36:53 +0800

    fix: 部署

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "129.28.201.134", user: "songcy", roles: %w{app db web}, my_property: :my_value
+server "49.233.81.195", user: "yueye", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :branch, ENV['BRANCH'] || :develop
