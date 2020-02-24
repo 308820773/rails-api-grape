32ce75e4ad406e506e5dab3d1e4bd59b56941d94
black <black@paimingjia.com>
2020-02-24 10:46:22 +0800

    fix: 添加 user_info

--- app/controllers/v1/wxa_grape.rb
+++ app/controllers/v1/wxa_grape.rb
@@ -2,6 +2,15 @@ module V1
   class WxaGrape < PubGrape
     params do
       requires :js_code, type: String, desc: '小程序临时登录凭证 code'
+
+      opontial :user_info, type: Hash do
+        opontial :nick_name, type: String
+        opontial :avatar_url, type: String
+        opontial :gender, type: String
+        opontial :province, type: String
+        opontial :city, type: String
+        opontial :country, type: String
+      end
     end
     get '/login' do
       client = Clients::WxaSign.execute(nil, js_code: params.js_code)
